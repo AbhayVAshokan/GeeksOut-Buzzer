c80a892f9bb5d2b4cc0e37031472ae41ddce800e
AbhayVAshokan <abhayvashokan@gmail.com>
2020-02-18 07:30:58 +0530

    added disqualified feature

--- buzzer_app/lib/widgets/button.dart
+++ buzzer_app/lib/widgets/button.dart
@@ -2,6 +2,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Button extends StatefulWidget {
   final teamName;
@@ -13,9 +14,40 @@ class Button extends StatefulWidget {
   _ButtonState createState() => _ButtonState();
 }
 
-class _ButtonState extends State<Button> {
+class _ButtonState extends State<Button> with WidgetsBindingObserver {
   final DatabaseReference database = FirebaseDatabase.instance.reference();
-    void playSound() {
+  int disQualifiedState = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _disqualifyCandidate();
+    }
+  }
+
+  void _disqualifyCandidate() {
+    setState(() {
+      disQualifiedState = 1;
+    });
+
+    database.child(widget.teamName).set({
+      'time': "Disqualified",
+    });
+  }
+
+  void playSound() {
     AudioCache cache = new AudioCache();
     cache.play('lib/assets/Buzzer_sound.mp3');
   }
@@ -72,16 +104,23 @@ class _ButtonState extends State<Button> {
         color: Colors.black,
         child: WillPopScope(
             child: Center(
-              child: RaisedButton(
-                shape: CircleBorder(),
-                color: Color.fromRGBO(128, 0, 0, 1),
-                child: Container(
-                  height: MediaQuery.of(context).size.width * 0.95,
-                ),
-                onPressed: () {
-                  sendData(widget.teamName);
-                },
-              ),
+              child: disQualifiedState == 0
+                  ? RaisedButton(
+                      shape: CircleBorder(),
+                      color: Color.fromRGBO(128, 0, 0, 1),
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.95,
+                      ),
+                      onPressed: () {
+                        sendData(widget.teamName);
+                      },
+                    )
+                  : Center(
+                      child: Text(
+                      'DISQUALIFIED',
+                      style: GoogleFonts.raleway(
+                          fontSize: 30, color: Colors.white),
+                    )),
             ),
             onWillPop: _onBackPressed),
       ),
